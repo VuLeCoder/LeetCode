cc22b95ef5c52ddaaeb670cdddea53477d9786cf
Vuz <155245548+VuLeCoder@users.noreply.github.com>
2024-10-12 20:13:49 +0700

    Create 02406.cpp

---
+++ 02406.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int minGroups(vector<vector<int>>& intervals) 
+    {
+        sort(intervals.begin(), intervals.end());
+        int n = intervals.size(), minG = 0;
+        priority_queue<int, vector<int>, greater<int>> group;
+        for(int i=0; i<n; ++i)
+        {
+            if(!group.empty() && group.top() < intervals[i][0])
+                group.pop();
+            group.push(intervals[i][1]);
+        }
+        return group.size();
+    }
+};
